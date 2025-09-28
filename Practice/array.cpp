7865dbb670364c7fb7844058aafb5d54d0f3a5c6
dev-shakilkhan <shakilkhancairo@gmail.com>
2025-09-28 08:01:00 +0600

    newfiles

---
+++ Practice/array.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int array[] = { 10,20,30,40,50,60};
+    int size = (sizeof(array))/(sizeof(array[0]));
+    for(int i = 0; i< size; i=i+2){
+        cout<<array[i]<<" ";
+    }
+return 0;
+}
